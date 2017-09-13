b51749692339cfc70cd097562c9515c0f6b80529
nigelboucher <nigelboucher@qq.com>
2017-09-13 15:50:46 +0800

    deep_portfolio

--- data/export.py
+++ data/export.py
@@ -15,43 +15,45 @@ import collections
 import datetime
 import dateutil.parser
 
+
 def export_time_series_data(files, usecols=6):
-	'''
-	input: times series csv of different assets and columns
-	assumptions: all have same column format
-	output log returns on then
-	'''
-	header = []
-	data_dict = {}
-	keys_dict = {}
-	for file in files:
-		file_data = np.genfromtxt(file, delimiter=",", skip_header=1, dtype="|S20", usecols = np.arange(usecols))
-		keys = file_data[:,0]
-		keys_dict[file] = keys
-		data_dict[file] = dict(zip(keys, file_data))
-	whitelisted_keys = reduce(set.intersection, (set(val) for val in keys_dict.values()))
-	keys_dict = {} #free memory
-	super_dict = collections.defaultdict(list)
-	for file in files:
-		header += [file + "_" + str(s) for s in np.arange(usecols)]
-		values = data_dict[file]
-		d = dict((k, values[k]) for k in whitelisted_keys if k in values)
-		for k, v in d.iteritems():
-			v = v[2:]
-			v = v.astype(np.float)
-			super_dict[k] += v.tolist()
-	new_file_name = "all_data.csv"
-	whitelisted_keys = sorted(whitelisted_keys, key=lambda x: dateutil.parser.parse(x))
-	with open(new_file_name, 'wb') as outfile:
-		writer = csv.writer(outfile)
-		writer.writerow(header)
-		for row in whitelisted_keys:
-			k = row
-			v = super_dict[k]
-			temp = [k] + v
-			writer.writerow(temp)
-	return super_dict
+    '''
+    input: times series csv of different assets and columns
+    assumptions: all have same column format
+    output log returns on then
+    '''
+    header = []
+    data_dict = {}
+    keys_dict = {}
+    for file in files:
+        file_data = np.genfromtxt(file, delimiter=",", skip_header=1, dtype="|S20", usecols=np.arange(usecols))
+        keys = file_data[:, 0]
+        keys_dict[file] = keys
+        data_dict[file] = dict(zip(keys, file_data))
+    whitelisted_keys = reduce(set.intersection, (set(val) for val in keys_dict.values()))
+    keys_dict = {}  # free memory
+    super_dict = collections.defaultdict(list)
+    for file in files:
+        header += [file + "_" + str(s) for s in np.arange(usecols)]
+        values = data_dict[file]
+        d = dict((k, values[k]) for k in whitelisted_keys if k in values)
+        for k, v in d.iteritems():
+            v = v[2:]
+            v = v.astype(np.float)
+            super_dict[k] += v.tolist()
+    new_file_name = "all_data.csv"
+    whitelisted_keys = sorted(whitelisted_keys, key=lambda x: dateutil.parser.parse(x))
+    with open(new_file_name, 'wb') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(header)
+        for row in whitelisted_keys:
+            k = row
+            v = super_dict[k]
+            temp = [k] + v
+            writer.writerow(temp)
+    return super_dict
+
 
 if __name__ == '__main__':
-	files = ["NIFTY_sort.csv", "NIFTY_F1_sort.csv"]
-	export_time_series_data(files,6)
+    files = ["NIFTY_sort.csv", "NIFTY_F1_sort.csv"]
+    export_time_series_data(files, 6)
--- data_gathering_scripts/sort_file_by_date.py
+++ data_gathering_scripts/sort_file_by_date.py
@@ -4,22 +4,23 @@ import csv
 import dateutil.parser
 import ipdb
 from collections import OrderedDict
+import os
 
-for filename in glob.glob('/Users/deep/development/deep_portfolio/data/*.txt'):
-	reader = csv.reader(open(filename, 'r'))
-	data_dict = {}
-	for row in reader:
-		#print(row)
-		date_time = dateutil.parser.parse(row[1] + "-" + row[2])
-		#if len(row) < 8:
-		#	row.append('0')
-		data_dict[date_time] = row
-	ordered = OrderedDict(sorted(data_dict.items(), key=lambda t:t[0]))
-	new_file_name =  filename.replace(".txt", "_sort.csv")
-	with open(new_file_name, 'wb') as outfile:
-		writer = csv.writer(outfile)
-		writer.writerow(["DateTime", "index", "OPEN", "HIGH", "LOW", "CLOSE"])
-		for row in ordered.items():
-			k = row[0]
-			v = row[1]
-			writer.writerow([k,v[0],v[3],v[4],v[5],v[6]])
+for filename in glob.glob(os.path.dirname(os.getcwd()) + "\\data\\*.txt"):
+    reader = csv.reader(open(filename, 'r'))
+    data_dict = {}
+    for row in reader:
+        # print(row)
+        date_time = dateutil.parser.parse(row[1] + "-" + row[2])
+        # if len(row) < 8:
+        #	row.append('0')
+        data_dict[date_time] = row
+    ordered = OrderedDict(sorted(data_dict.items(), key=lambda t: t[0]))
+    new_file_name = filename.replace(".txt", "_sort.csv")
+    with open(new_file_name, 'wb') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(["DateTime", "index", "OPEN", "HIGH", "LOW", "CLOSE"])
+        for row in ordered.items():
+            k = row[0]
+            v = row[1]
+            writer.writerow([k, v[0], v[3], v[4], v[5], v[6]])
